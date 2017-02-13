b0613fa31d261991c9c504d15a4cedbbaefbf11e
murphypicard <ayaghsizian@gmail.com>
2017-02-13 13:57:36 -0500

    commented out a function on bottom and added in start script

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ app.put("/api/dancers/:_id", function(req, res){
 
 
 //according to line 13
-app.listen(process.env.PORT || 3001 ), function(){
-  console.log("*** *** *** !!! !!! Ara, it's on 3001 maaayybe !!! !!! *** *** ***");
-});
+app.listen(process.env.PORT || 3001 )
+// , function(){
+//   console.log("*** *** *** !!! !!! Ara, it's on 3001 maaayybe !!! !!! *** *** ***");
+// };
 // app.listen(app.get("port")
